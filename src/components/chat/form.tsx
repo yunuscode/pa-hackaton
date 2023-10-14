f24443616cf0b652767cd452443a9e7e0e881272
yunuscode <yunuzmuhammad1@gmail.com>
2023-10-15 00:20:05 +0500

    FIX: env

--- src/components/chat/form.tsx
+++ src/components/chat/form.tsx
@@ -9,24 +9,31 @@ import {
 
 export default function ChatForm() {
   return (
-    <form className="mt-auto relative">
-      <input
-        className="border w-full resize-none px-4 py-3"
-        placeholder="Send message..."
-        name="message"
-      />
-      <div className="absolute right-0 top-4 sm:right-4">
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button type="submit" size="icon">
-                <IconArrowElbow />
-                <span className="sr-only">Send message</span>
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent>Send message</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+    <form className="mt-auto">
+      <div className="relative">
+        <input
+          className="border w-full resize-none px-4 py-3"
+          placeholder="Send message..."
+          name="message"
+        />
+        <div
+          className="absolute"
+          style={{
+            transform: "translate(-50%, 0)",
+          }}
+        >
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button type="submit" size="icon">
+                  <IconArrowElbow />
+                  <span className="sr-only">Send message</span>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>Send message</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
       </div>
     </form>
   );
